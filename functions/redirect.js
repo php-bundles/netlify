ba5f8e5f38037c7c7d58b7cbc72817ac53510ccd
Dmitry Khaperets <khaperets@gmail.com>
2021-09-28 17:28:59 +0300

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,15 +1,23 @@
 exports.handler = async event => {
-  const path = event.queryStringParameters.url || ''
+  const path = event.path || ''
   const referer = event.headers.referer || ''
   const userAgent = (event.headers['user-agent'] || '').toLowerCase()
   
   console.log('path: ', path, '; referer: ', referer, '; userAgent: ', userAgent);
 
-  //if (referer.includes('facebook') && userAgent.includes('android')) {
-  return {
-    statusCode: 301,
-    headers: {
-      location: 'https://embroiderio.com' + path
+  if (referer.includes('facebook') && userAgent.includes('android')) {
+    return {
+      statusCode: 301,
+      headers: {
+        location: 'https://embroiderio.com' + path
+      }
+    }
+  } else {
+    return {
+      statusCode: 301,
+      headers: {
+        location: 'https://embroiderio.com' + path
+      }
     }
   }
 }
