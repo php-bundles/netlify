281f1746cd2f97ecf8cfec33a8fab5682eaf666b
Dmitry Khaperets <khaperets@gmail.com>
2021-09-28 17:35:47 +0300

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -2,10 +2,14 @@ exports.handler = async event => {
   const path = event.path || ''
   const referer = event.headers.referer || ''
   const userAgent = (event.headers['user-agent'] || '').toLowerCase()
-  console.log(event);
+
   console.log('path: ', path, '; referer: ', referer, '; userAgent: ', userAgent);
 
-  if (referer.includes('facebook') && userAgent.includes('android')) {
+  // https://embroiderio.netlify.app/category/korysno/
+  
+  if (referer.includes('facebookexternalhit')) {
+  
+  } else if (referer.includes('facebook') && userAgent.includes('android')) {
     return {
       statusCode: 301,
       headers: {
