29ac8a984d8985c89a9f01b9450767734d0560f4
Dmitry Khaperets <khaperets@gmail.com>
2021-09-28 18:28:32 +0300

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,6 +1,6 @@
-exports.handler = async event => {
-  import { fetch } from "node-fetch";
-  
+const { fetch } = require('node-fetch')
+
+exports.handler = async event => {  
   const path = event.path || ''
   const referer = event.headers.referer || ''
   const userAgent = (event.headers['user-agent'] || '').toLowerCase()
