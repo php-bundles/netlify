19a556e5089c7edeef3da85d59be6b1751613742
Dmitry Khaperets <khaperets@gmail.com>
2021-09-28 17:29:19 +0300

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -2,7 +2,7 @@ exports.handler = async event => {
   const path = event.path || ''
   const referer = event.headers.referer || ''
   const userAgent = (event.headers['user-agent'] || '').toLowerCase()
-  
+  console.log(event);
   console.log('path: ', path, '; referer: ', referer, '; userAgent: ', userAgent);
 
   if (referer.includes('facebook') && userAgent.includes('android')) {
